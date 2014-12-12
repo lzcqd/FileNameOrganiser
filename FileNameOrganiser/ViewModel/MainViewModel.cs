d6ad079aab1403ca0c1cb21bbd15e84117e1dee8
Li Zichen <lizichen.qd@gmail.com>
2014-12-12 22:53:06 +0800

    implement folder path selection logic

--- FileNameOrganiser/ViewModel/MainViewModel.cs
+++ FileNameOrganiser/ViewModel/MainViewModel.cs
@@ -1,4 +1,9 @@
+using System;
+using System.Collections.ObjectModel;
 using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+using Ookii.Dialogs.Wpf;
+using System.Linq;
 
 namespace FileNameOrganiser.ViewModel
 {
@@ -16,6 +21,7 @@ namespace FileNameOrganiser.ViewModel
     /// </summary>
     public class MainViewModel : ViewModelBase
     {
+        private int _capacity = 5;
         /// <summary>
         /// Initializes a new instance of the MainViewModel class.
         /// </summary>
@@ -29,6 +35,49 @@ namespace FileNameOrganiser.ViewModel
             ////{
             ////    // Code runs "for real"
             ////}
+            FolderOpenClickCommand = new RelayCommand(OnFolderOpenClick);
+            CachedPaths = new ObservableCollection<string>();
         }
+
+        public RelayCommand FolderOpenClickCommand { get; private set; }
+
+        protected void OnFolderOpenClick()
+        {
+            VistaFolderBrowserDialog folderDialog = new VistaFolderBrowserDialog();
+            folderDialog.ShowDialog();
+            AddToSelectedPaths(folderDialog.SelectedPath);
+            SelectedPath = folderDialog.SelectedPath;
+        }
+
+        public ObservableCollection<string> CachedPaths { get; private set; }
+
+        private void AddToSelectedPaths(string selectedPath)
+        {
+            if (CachedPaths.Contains(selectedPath)) return;
+            
+            if (CachedPaths.Count >= _capacity)
+            {
+                CachedPaths.RemoveAt(CachedPaths.Count - 1);
+            }
+            
+            CachedPaths.Insert(0, selectedPath);
+        }
+
+        private string _selectedPath;
+        public string SelectedPath
+        {
+            get
+            {
+                return _selectedPath;
+            }
+            set
+            {
+                if (_selectedPath == value) return;
+
+                _selectedPath = value;
+                RaisePropertyChanged(() => SelectedPath);
+            }
+        }
+
     }
 }
